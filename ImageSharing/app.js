4e011e433ede20a4fa39589fd4656f70a4fbc134
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2024-11-25 21:29:19 +0530

    work on fronted

--- ImageSharing/app.js
+++ ImageSharing/app.js
@@ -12,7 +12,6 @@ app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-    // res.send("App Work");
     res.render("index.ejs")
 });
 
